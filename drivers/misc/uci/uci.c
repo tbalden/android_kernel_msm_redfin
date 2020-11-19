80663ce9261d111de2bf2475329a0ae6494a8db6
tbalden <illespal@gmail.com>
2020-11-19 10:34:56 -0800

    uci: set to real calls, all drivers ready

--- drivers/misc/uci/uci.c
+++ drivers/misc/uci/uci.c
@@ -26,9 +26,9 @@
 //#define UCI_LOG_DEBUG
 
 // comment these when callbacks implemented in drivers
-#define EMPTY_CALLBACKS_TORCH
-#define EMPTY_CALLBACKS_VIB
-#define EMPTY_CALLBACKS_KCAL
+//#define EMPTY_CALLBACKS_TORCH
+//#define EMPTY_CALLBACKS_VIB
+//#define EMPTY_CALLBACKS_KCAL
 #define EMPTY_CALLBACKS_LED_FRONT
 #define EMPTY_CALLBACKS_LED_BACK
 
