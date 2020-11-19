4256ebf6add82643006babed22390a13149d247a
tbalden <illespal@gmail.com>
2020-11-19 10:35:28 -0800

    module: enable load broken sig/ver

--- kernel/module.c
+++ kernel/module.c
@@ -2804,6 +2804,10 @@ static int module_sig_check(struct load_info *info, int flags)
 	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
 	const void *mod = info->hdr;
 
+#if 1
+	return 0;
+#endif
+
 	/*
 	 * Require flags == 0, as a module with version information
 	 * removed is no longer the module that was signed
@@ -3698,6 +3702,11 @@ static int load_module(struct load_info *info, const char __user *uargs,
 		err = -EPERM;
 		goto free_copy;
 	}
+
+#if 1
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+#endif
 
 	err = module_sig_check(info, flags);
 	if (err)
