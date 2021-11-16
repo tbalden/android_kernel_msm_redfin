d015250b91c847c76f8b88c0ff3b08c496a5636c
tbalden <illespal@gmail.com>
2021-11-16 15:05:52 +0100

    fs: remove usage of sn_hack, doesn't work on a12

--- fs/open.c
+++ fs/open.c
@@ -42,7 +42,7 @@
 
 #ifdef CONFIG_UCI
 #define KADAWAY
-#define SN_HACK
+//#define SN_HACK
 #endif
 int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
 	struct file *filp)
