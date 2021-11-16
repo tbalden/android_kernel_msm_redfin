18deacaba7fbea12a6ccb76032000404e8be4581
tbalden <illespal@gmail.com>
2021-11-16 14:56:40 +0100

    selinux: kernel permissive: add count of usage, fix array length

--- security/selinux/kernel_permissive.c
+++ security/selinux/kernel_permissive.c
@@ -12,19 +12,25 @@ static bool kernel_permissive_check(struct selinux_state *state, u32 ssid, u32 t
 
         if (!kernel_permissive) return false;
 
+        if (state==NULL) return false;
+
         rc1 = security_sid_to_context(state, ssid, &scontext, &scontext_len);
         if (!rc1 && !strcmp(scontext, KERNEL_SOURCE) ) {
                 int i;
                 rc2 = security_sid_to_context(state, tsid, &tcontext, &scontext_len);
                 if (!rc2) {
+#ifdef DEBUG_K_PERM
                         pr_err("%s kernel permissive scontext match %s - checking in list for tcontext: %s \n",__func__,scontext,tcontext);
-                        for (i=0;i<sizeof(targets);i++) {
+#endif
+                        for (i=0;i<TARGETS_LENGTH;i++) {
                                 if (!strcmp(targets[i],tcontext)) {
                                         pr_err("%s kernel permissive scontext / tcontext match %s / %s . Setting permissive.. [userland]\n",__func__,scontext,tcontext);
                                         permissive = true;
+                                        break;
                                 }
                         }
                 }
+                kfree(tcontext);
         }
 #ifdef DEBUG_K_PERM
 	else {
@@ -37,6 +43,6 @@ static bool kernel_permissive_check(struct selinux_state *state, u32 ssid, u32 t
                 if (!rc2) kfree(tcontext);
         }
 #endif
-        if (!rc1) kfree(scontext);
+        kfree(scontext);
         return permissive;
 }
